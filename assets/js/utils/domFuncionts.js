dfe8aaab4aaa0b62808f46cf970abb6eaab0f1f0
Beatriz Freitas <91092548+beatrizfrtso@users.noreply.github.com>
2022-07-19 11:24:34 -0300

    Add files via upload

---
+++ assets/js/utils/domFuncionts.js
@@ -0,0 +1,35 @@
+export function addTask(db, title= "") {
+    const task = document.createElement("div");
+    task.classList.add("task");
+    task.setAttribute("id", db.id);
+
+    const taskBody = `
+    <div> <input type="checkbox" id="check_${db.id}" /></div>
+    <div>
+        <div><span class="title-task">${title?title:db.title}</span></div>
+    </div>
+        <div>Star</div>
+    `;
+
+    task.innerHTML = taskBody;
+    document.querySelector(".tasks").appendChild(task);
+  }
+
+  export function removeTask() {
+    alert("Removendo uma nova tarefa");
+  }
+
+  export function updateTask() {
+    alert("Alterando uma nova tarefa");
+  }
+// colocamos chocolate apenas para explicar que pode ser qualquer nome.
+  export function getAllTasks(chocolate) {
+    chocolate.forEach((item) => {
+      addTask(item);
+    });
+  }
+
+  export function getTaskById() {
+    alert("Exibindo uma tarefa");
+  }
+  
---
+++ assets/js/utils/write.js
@@ -0,0 +1,4 @@
+export const escreve = (x) => {
+    console.log("Frase: " + x);
+  };
+  
